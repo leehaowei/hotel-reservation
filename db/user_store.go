f67d82fcc79e6e5d312220334e02a70117e999b6
Haowei Lee <haoweilee.tpe@gmail.com>
2024-04-17 20:27:26 +0800

    creating and validating user

--- db/user_store.go
+++ db/user_store.go
@@ -16,6 +16,8 @@ const (
 // UserStore act as an abstraction layer of the db (dao/store)
 type UserStore interface {
 	GetUserByID(context.Context, string) (*types.User, error)
+	GetUsers(context.Context) ([]*types.User, error)
+	InsertUsers(context.Context, *types.User) (*types.User, error)
 }
 
 type MongoUserStore struct {
@@ -28,6 +30,27 @@ func NewMongoUSerStore(client *mongo.Client) *MongoUserStore {
 		client: client,
 		coll:   client.Database(DBNAME).Collection(userColl),
 	}
+}
+
+func (s *MongoUserStore) InsertUsers(ctx context.Context, user *types.User) (*types.User, error) {
+	res, err := s.coll.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
+
+func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
+	cur, err := s.coll.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	var users []*types.User
+	if err := cur.All(ctx, &users); err != nil {
+		return []*types.User{}, nil
+	}
+	return users, nil
 }
 
 func (s *MongoUserStore) GetUserByID(ctx context.Context, id string) (*types.User, error) {
